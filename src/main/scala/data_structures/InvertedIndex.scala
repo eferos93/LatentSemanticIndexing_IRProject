346895f7ce0de533284dd2d4b4cffdfb84a6b869
eferos93 <eros.fabrici@gmail.com>
2021-09-07 16:38:35 +0200

    inverted index ready

--- src/main/scala/data_structures/InvertedIndex.scala
+++ src/main/scala/data_structures/InvertedIndex.scala
@@ -3,20 +3,14 @@ package data_structures
 
 import sparkSession.implicits._
 
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.functions.sum
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, Dataset, Row}
 
 class InvertedIndex {
-  val schema: StructType = StructType(
-    Array(
-      StructField("term", StringType, nullable = false),
-      StructField("documentId", LongType, nullable = false),
-      StructField("termFrequency", IntegerType, nullable = false)
-    )
-  )
-
-  var dictionary: DataFrame = sparkSession.createDataFrame(sparkContext.emptyRDD[Row], schema)
+  type Term = String
+  type DocumentId = Long
+  type TermFrequency = Long
+  var dictionary: Map[Term, Map[DocumentId, TermFrequency]] = Map.empty
 }
 
 object InvertedIndex {
@@ -25,18 +19,20 @@ object InvertedIndex {
     val tokens = removeStopWords(
       corpus.map(movie => clean(movie.plot)).toDF("tokens")
     )
-
     invertedIndex.dictionary =
       tokens.as[Seq[String]].rdd
-        .zipWithIndex
+        .zipWithIndex // zip with document Id
         .flatMap {
           case (tokens, documentId) => tokens.map(term => (term, documentId, 1))
         }
         .toDF("term", "documentId", "count")
         .groupBy("term", "documentId")
         .agg(sum("count").as("termFrequency"))
-        .cache()
-
+        .as[(String, Long, Long)] // convert DataFrame to Dataset[(String, Long, Long)]
+        .collect() // collect the dataset and send it to the memory of the driver application
+        .groupBy(_._1).mapValues {
+          _.map { case (_, docId, termFrequency) => (docId, termFrequency) }.toMap
+        }
     invertedIndex
   }
 
