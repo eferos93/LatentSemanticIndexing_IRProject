0ad86ad98afd3465f47e6808e9ad65ddddb49c94
eferos93 <eros.fabrici@gmail.com>
2021-09-07 18:10:19 +0200

    added instance method

--- src/main/scala/data_structures/InvertedIndex.scala
+++ src/main/scala/data_structures/InvertedIndex.scala
@@ -6,15 +6,17 @@ import sparkSession.implicits._
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.functions.sum
 
-class InvertedIndex(val dictionary: Map[String, Map[Long, Long]])
+class InvertedIndex(val dictionary: Map[String, Map[Long, Long]]) {
+  def keys(): Seq[String] = dictionary.keys.toSeq.sorted
+}
 
 object InvertedIndex {
   def apply(corpus: Dataset[Movie]): InvertedIndex = {
     val dictionary: Map[String, Map[Long, Long]] =
       removeStopWords(
         corpus.map(movie => clean(movie.plot)).toDF("tokens")
-      ).as[Seq[String]].rdd //convert to Dataset[Seq[String]] then to RDD
-        .zipWithIndex // zip with document Id
+      ).as[Seq[String]].rdd //convert to Dataset[Seq[String]] then to RDD[Seq[String]]
+        .zipWithIndex // zip with documentId
         .flatMap {
           case (tokens, documentId) => tokens.map(term => (term, documentId, 1))
         }
@@ -22,8 +24,8 @@ object InvertedIndex {
         .groupBy("term", "documentId") // groupBy together with agg, is a relational style aggregation
         .agg(sum("count").as("termFrequency"))
         .as[(String, Long, Long)] // convert DataFrame to Dataset[(String, Long, Long)]
-        .collect() // collect the dataset and send it to the memory of the driver application then convert it to a HashMap
-        .groupBy(_._1).mapValues {
+        .collect() // collect the dataset as an Array[(String, Long, Long)] and send it to the memory of the driver application
+        .groupBy(_._1).mapValues { // convert the array to a Map of Maps to a HashMap
           _.map { case (_, docId, termFrequency) => (docId, termFrequency) }.toMap
         }
     new InvertedIndex(dictionary)
