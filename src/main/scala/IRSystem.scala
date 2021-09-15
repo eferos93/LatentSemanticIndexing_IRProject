cbbe4e94f00f04d9c2ccb2d7a4a232eb78fb9065
eferos93 <eros.fabrici@gmail.com>
2021-09-15 14:33:18 +0200

    added method for printing query result

--- src/main/scala/IRSystem.scala
+++ src/main/scala/IRSystem.scala
@@ -10,25 +10,30 @@ class IRSystem(corpus: Dataset[Movie],
                vocabulary: Dataset[String],
                U: DenseMatrix, sigma: DenseVector, V: DenseMatrix) {
 
-  def mapQueryVector(queryVector: Vector): DenseVector = {
+  private def mapQueryVector(queryVector: Vector): DenseVector = {
     val inverseDiagonalSigma = Matrices.diag(new DenseVector(sigma.toArray.map(math.pow(_, -1))))
     inverseDiagonalSigma.multiply(U.transpose).multiply(queryVector)
   }
 
-  def buildQueryVector(textQuery: String): Vector = {
-    val tokens = removeStopWords(List(clean(textQuery)).toDF("tokens")).first().getAs[Seq[String]](0)
+  private def buildQueryVector(textQuery: String): Vector = {
+    val tokens = removeStopWords(
+      List(clean(textQuery)).toDF("tokens")
+    ).first().getAs[Seq[String]](0)
     val asRDD = vocabulary.rdd.zipWithIndex.map { case (word, index) => (index.toInt, tokens.count(_ == word).toDouble) }
     val queryVector = Vectors.sparse(vocabulary.count.toInt, asRDD.collect)
     mapQueryVector(queryVector)
   }
 
-  def answerQuery(textQuery: String, top: Int = 5): Seq[(Movie, Double)] = {
+  private def answerQuery(textQuery: String, top: Int): Seq[(Movie, Double)] = {
     val queryVector = buildQueryVector(textQuery)
     V.rowIter.toStream
       .map(queryVector.dot).zipWithIndex.sortBy(_._1)
       .map { case (score, documentId) => (corpus.where($"id" === documentId + 1).first, score) }
       .take(top)
   }
+
+  def query(query: String, top: Int = 5): Unit =
+    println(answerQuery(query, top).mkString("\n"))
 }
 
 object IRSystem {
@@ -42,7 +47,7 @@ object IRSystem {
     initializeIRSystem(corpus, termDocumentMatrix, k)
   }
 
-  def initializeIRSystem(corpus: Dataset[Movie], termDocumentMatrix: TermDocumentMatrix, k: Int): IRSystem = {
+  private def initializeIRSystem(corpus: Dataset[Movie], termDocumentMatrix: TermDocumentMatrix, k: Int): IRSystem = {
     val singularValueDecomposition = termDocumentMatrix.computeSVD(k)
     val U = singularValueDecomposition.U
     val UasDense = new DenseMatrix(U.numRows.toInt, U.numCols.toInt, U.rows.flatMap(_.toArray).collect, isTransposed = false)
