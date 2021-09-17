6d2f1fc511088de07b017c60a790a6dcc49e9f39
eferos93 <eros.fabrici@gmail.com>
2021-09-17 18:02:05 +0200

    save matrices function done

--- src/main/scala/IRSystem.scala
+++ src/main/scala/IRSystem.scala
@@ -3,8 +3,11 @@ package org.ir.project
 import data_structures.{Movie, TermDocumentMatrix}
 import sparkSession.implicits._
 
-import org.apache.spark.ml.linalg.{DenseMatrix, DenseVector, Matrices, Vector, Vectors}
+import org.apache.spark.ml.linalg.{DenseMatrix, DenseVector, Matrices, Matrix, Vector, Vectors}
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+import org.apache.spark.mllib.linalg.{Vectors => OldVectors}
 import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.types.{ArrayType, DoubleType, StructField, StructType}
 import org.apache.spark.storage.StorageLevel
 
 class IRSystem(val corpus: Dataset[Movie],
@@ -27,7 +30,7 @@ class IRSystem(val corpus: Dataset[Movie],
   private def answerQuery(textQuery: String, top: Int): Seq[(Movie, Double)] = {
     val queryVector = buildQueryVector(textQuery)
     sparkContext.parallelize(V.rowIter.toSeq).zipWithIndex
-      .map { case (vector, documentId) => (documentId, -queryVector.dot(vector))}
+      .map { case (vector, documentId) => (documentId, -queryVector.dot(vector)) }
       .sortBy(_._2, ascending = false) // descending sorting
       .take(top)
       .map { case (documentId, score) => (corpus.where($"id" === documentId).first, score)}
@@ -35,6 +38,13 @@ class IRSystem(val corpus: Dataset[Movie],
 
   def query(query: String, top: Int = 5): Unit =
     println(answerQuery(query, top).mkString("\n"))
+
+  def saveIrSystem(): Unit = {
+    sparkContext.parallelize(U.rowIter.toSeq, numSlices = 1).saveAsTextFile("matrices/U")
+    sparkContext.parallelize(V.rowIter.toSeq, numSlices = 1).saveAsTextFile("matrices/V")
+    sparkContext.parallelize(Seq(sigma), numSlices = 1).saveAsTextFile("matrices/s")
+  }
+
 
 }
 
@@ -47,6 +57,19 @@ object IRSystem {
   def apply(corpus: Dataset[Movie], pathToIndex: String, k: Int): IRSystem = {
     val termDocumentMatrix = TermDocumentMatrix(pathToIndex)
     initializeIRSystem(corpus, termDocumentMatrix, k)
+  }
+
+  def apply(corpus: Dataset[Movie], pathToIndex: String, pathToMatrices: String, k: Int) = {
+    val U = readMatrix(s"$pathToMatrices/U/part-00000").toDense.transpose
+    val V = readMatrix(s"$pathToMatrices/V/part-00000").toDense.transpose
+    U
+//    val sigma =
+  }
+
+  private def readMatrix(pathToMatrix: String): Matrix = {
+    val matrixAsRDD = sparkContext.textFile(pathToMatrix).map(line => OldVectors.parse(line))
+    val asRowMatrix = new RowMatrix(matrixAsRDD)
+    Matrices.dense(asRowMatrix.numRows.toInt, asRowMatrix.numCols.toInt, asRowMatrix.rows.flatMap(_.toArray).collect)
   }
 
   private def initializeIRSystem(corpus: Dataset[Movie], termDocumentMatrix: TermDocumentMatrix, k: Int): IRSystem = {
