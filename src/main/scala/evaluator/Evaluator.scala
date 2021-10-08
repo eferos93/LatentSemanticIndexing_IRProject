dcfa26926188bd3f22f29a5c1d82dba16bb7da73
Eros Fabrici <eros.fabrici@gmail.com>
2021-10-08 16:37:20 +0200

    extract method

--- src/main/scala/evaluator/Evaluator.scala
+++ src/main/scala/evaluator/Evaluator.scala
@@ -11,13 +11,19 @@ import scala.language.implicitConversions
 
 case class Evaluator[T <: Document](irSystem: IRSystem[T], queryRelevance: DataFrame) {
 
-  def averagePrecision(query: String, relevanceSet: mutable.WrappedArray[Int]): Double = {
+  private def precision(relevanceSet: mutable.WrappedArray[Int],
+                        documentIds: Seq[Long],
+                        numberOfDocuments: Int): Double = {
     implicit def bool2int(b: Boolean): Int = if (b) 1 else 0
+    documentIds.take(numberOfDocuments).map(relevanceSet.contains(_): Int).sum.toDouble / numberOfDocuments
+  }
+
+  def averagePrecision(query: String, relevanceSet: mutable.WrappedArray[Int]): Double = {
     val documentIds = irSystem.answerQuery(query, relevanceSet.length)
       .map(_._1.id + 1) // + 1 cause ids internally starts from 0
 
     (1 to relevanceSet.length).map { index =>
-      documentIds.take(index).map(relevanceSet.contains(_): Int).sum / index // precision
+      precision(relevanceSet, documentIds, index) // precision
     }.sum / relevanceSet.length
   }
 
