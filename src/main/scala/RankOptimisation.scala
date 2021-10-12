3215f58936902a5268fadcbf21dc389be4ce361d
Eros Fabrici <eros.fabrici@gmail.com>
2021-10-12 17:13:23 +0200

    added RankOptimisation.scala

---
+++ src/main/scala/RankOptimisation.scala
@@ -0,0 +1,27 @@
+package org.ir.project
+
+import data_structures.CranfieldDocument
+import evaluator.Evaluator
+
+import org.apache.spark.sql.Dataset
+
+import java.nio.file.{Files, Paths}
+
+object RankOptimisation extends App {
+  val corpus: Dataset[CranfieldDocument] = readCranfieldCorpus()
+  val queryAndRelevanceSets: Array[(String, Array[Long])] = readQueryRelevanceCranfield()
+  (50 to 500 by 25).map { singularValues =>
+    val irSystem = if (Files.exists(Paths.get("/index"))) {
+      IRSystem(corpus, singularValues, "index", tfidf = false)
+    } else {
+      IRSystem(corpus, singularValues, tfidf = false)
+    }
+    val eval = Evaluator(irSystem, queryAndRelevanceSets)
+    (
+      s"n° of singular values: $singularValues",
+      s"MAP: ${eval.meanAveragePrecision()}",
+      s"NDCG at 10: ${eval.normalisedDiscountedCumulativeGainAt(10)}"
+    )
+  }.foreach(println(_))
+
+}
