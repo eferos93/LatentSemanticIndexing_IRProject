70c552a75db98c1f902769b88f06ce85e777b2a7
Eros Fabrici <eros.fabrici@gmail.com>
2021-10-13 14:12:48 +0200

    added comments in package.scala

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -14,7 +14,6 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.ir.project.data_structures.{Movie, NplDocument}
 
 
-
 package object project {
 
   lazy val sparkConfiguration: SparkConf = new SparkConf()
@@ -28,30 +27,23 @@ package object project {
 
   import sparkSession.implicits._
 
-  //  utility function
-  def time[R](block: => R): R = {
-    val t0 = System.nanoTime()
-    val result = block // call-by-name
-    val t1 = System.nanoTime()
-    println("Elapsed time: " + (t1 - t0).toDouble / 1000000000 + "seconds")
-    result
-  }
-
- // function adapted to a column function: see its application at line 5c4
-  val normaliseText: UserDefinedFunction = udf { text: String =>
-    //    pattern is:
-    //    - ^\\w : not a word
-    //    - ^\\s : not a  space
-    //    - ^- : not a -
-    text.replaceAll("[^\\w^\\s^-]", "").toLowerCase
-  }
 
   def pipelineClean(dataSet: Dataset[_],
                     columnToClean: ColumnName = $"description",
                     extraColumns: Seq[String] = Seq("id")
-                    ): DataFrame = {
+                   ): DataFrame = {
+    // function adapted to a column function
+    val normaliseText: UserDefinedFunction = udf { text: String =>
+      //    pattern is:
+      //    - ^\\w : not a word
+      //    - ^\\s : not a  space
+      //    - ^- : not a -
+      text.replaceAll("[^\\w^\\s^-]", "").toLowerCase
+    }
+
     val filteredCorpus = dataSet.withColumn("normalisedDescription", normaliseText(columnToClean))
 
+    // we define some transformers
     val documentAssembler = new DocumentAssembler()
       .setInputCol("normalisedDescription")
       .setOutputCol("text")
@@ -66,6 +58,7 @@ package object project {
       .setInputCols("stem")
       .setOutputCol("stemNoStopWords")
 
+    //define the pipeline of execution
     val pipeline = new Pipeline().setStages(Array(
       documentAssembler,
       tokenizer,
@@ -73,8 +66,9 @@ package object project {
       stopWordsCleaner
     ))
 
+    //transform the data
     pipeline.fit(filteredCorpus).transform(filteredCorpus)
-      .selectExpr(extraColumns :+ "stemNoStopWords.result":_*)
+      .selectExpr(extraColumns :+ "stemNoStopWords.result": _*)
       .withColumnRenamed("result", "tokens")
   }
 
@@ -93,7 +87,7 @@ package object project {
         .option("header", headerPresent).csv(filepath)
 
     columnsToSelect match {
-      case Some(columns) => data.select(columns:_*)
+      case Some(columns) => data.select(columns: _*)
       case None => data
     }
   }
@@ -109,35 +103,37 @@ package object project {
                       filepathDescriptions: String = "data/MovieSummaries/plot_summaries.txt"): Dataset[Movie] = {
     val titles =
       readData(filepathTitles, columnsToSelect = Option(Seq($"_c0", $"_c2")))
-      .toDF("internalId", "title")
+        .toDF("internalId", "title") // to Dataframe
     val descriptions = readData(filepathDescriptions).toDF("movieId", "description")
 
     titles
       .join(descriptions, titles("id") === descriptions("movieId"))
-      .select( "title", "description")
-      .orderBy("title").rdd
+      .select("title", "description")
+      .orderBy("title").rdd // covert it to RDD[Row]
       .zipWithIndex.map { case (Row(title: String, description: String), id) => (id, title, description) }
-      .toDF("id", "title", "description")
-      .as[Movie].persist(StorageLevel.MEMORY_ONLY_SER)
+      .toDF("id", "title", "description") // back to Dataframe
+      .as[Movie].persist(StorageLevel.MEMORY_ONLY_SER) //convert it to Dataset[Movie]
   }
 
   /**
    * Data can be downloaded with the link below, but use the data in this repo, as there were some problems
-   *  with the file rlv-ass http://ir.dcs.gla.ac.uk/resources/test_collections/npl/
+   * with the file rlv-ass http://ir.dcs.gla.ac.uk/resources/test_collections/npl/
    */
   def readNplCorpus(path: String = "data/npl/doc-text"): Dataset[NplDocument] = {
-    val df: DataFrame = sparkSession.read.option("lineSep", "   /\n").text(path)
-    val columnsSplit = split(df("value"), "\n")
+    val df: DataFrame = sparkSession.read.option("lineSep", "   /\n").text(path) // Dataframe with one single column "value"
+    val columnsSplit = split(df("value"), "\n") //split the column
     df.withColumn("id", columnsSplit.getItem(0).cast(IntegerType) - 1) //I want indexes to start from 0 (for matrix)
       .withColumn("description", columnsSplit.getItem(1))
       .select("id", "description")
-      .as[NplDocument].persist(StorageLevel.MEMORY_ONLY_SER)
+      .as[NplDocument].persist(StorageLevel.MEMORY_ONLY_SER) // covert it to Dataset[NplDocument]
   }
 
   def readQueryAndRelevance(pathToQueries: String = "data/npl/query-text",
                             pathToRelevance: String = "data/npl/rlv-ass"): Array[(String, Array[Long])] = {
+    //both Dataframes have one single column
     var queryDf = sparkSession.read.option("lineSep", "/\n").text(pathToQueries)
     var relevanceDf = sparkSession.read.option("lineSep", "/\n").text(pathToRelevance)
+    // split their columns with pattern limited 2 times
     val queryColumnsSplit = split(queryDf("value"), "\n", 2)
     val relevanceColumnSplit = split(relevanceDf("value"), "\n", 2)
     queryDf =
@@ -151,12 +147,14 @@ package object project {
         .withColumn("relevanceSet", relevanceColumnSplit.getItem(1))
         .select("queryId", "relevanceSet")
 
+    // column function to convert the relevance set from String to Array[Int]
     val stringToList: UserDefinedFunction = udf { relevanceSet: String =>
       // withFilter is a MonadicFilter, thus it doesn't return a new collection like filter(),
       // but it restricts the domain of the subsequent transformations. In simple words, it is lazy
       relevanceSet.split("[\n|\\s+]").withFilter(_.nonEmpty).map(_.toInt)
     }
 
+    // join the two dataframes and return a Array of tuples (query, relevanceSet)
     queryDf
       .join(relevanceDf, queryDf("id") === relevanceDf("queryId"))
       .withColumn("relevanceList", stringToList($"relevanceSet"))
