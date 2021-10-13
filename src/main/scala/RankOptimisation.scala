2623fd65e6653fca58caad06925195fd9f125412
Eros Fabrici <eros.fabrici@gmail.com>
2021-10-13 11:44:57 +0200

    implemented rank optim

--- src/main/scala/RankOptimisation.scala
+++ src/main/scala/RankOptimisation.scala
@@ -8,22 +8,44 @@ import org.apache.spark.sql.Dataset
 import java.nio.file.{Files, Paths}
 
 object RankOptimisation extends App {
+  def evaluation(corpus: Dataset[CranfieldDocument],
+                 queryAndRelevanceSets: Array[(String, Array[Long])],
+                 tfidf: Boolean) = {
+    val results =
+      (100 to 700 by 100).map { singularValues =>
+        println(s"Testing with $singularValues singular values")
+        val irSystem = if (Files.exists(Paths.get("index"))) {
+          println("index found")
+          IRSystem(corpus, singularValues, "index", tfidf = tfidf)
+        } else {
+          println("index not found")
+          IRSystem(corpus, singularValues, tfidf = tfidf)
+        }
+        val eval = Evaluator(irSystem, queryAndRelevanceSets)
+        (
+          singularValues,
+          eval.meanAveragePrecision(),
+          (eval.ndcgAt(5), eval.ndcgAt(10), eval.ndcgAt(20), eval.ndcgAt(40))
+        )
+      }
+
+    results.map {
+      case (k, map, ndcg) =>
+        (
+          s"n of singular values: $k",
+          s"MAP: $map",
+          s"ndcg@ 5, 10, 20, 40: ${ndcg.toString}"
+        )
+    }.foreach(println(_))
+
+    results
+  }
+
   val corpus: Dataset[CranfieldDocument] = readCranfieldCorpus()
   val queryAndRelevanceSets: Array[(String, Array[Long])] = readQueryRelevanceCranfield()
-  (50 to 500 by 25).map { singularValues =>
-    val irSystem = if (Files.exists(Paths.get("/index"))) {
-      println("index not found")
-      IRSystem(corpus, singularValues, tfidf = false)
-    } else {
-      println("index found")
-      IRSystem(corpus, singularValues, "index", tfidf = false)
-    }
-    val eval = Evaluator(irSystem, queryAndRelevanceSets)
-    (
-      s"n° of singular values: $singularValues",
-      s"MAP: ${eval.meanAveragePrecision()}",
-      s"NDCG at 10: ${eval.normalisedDiscountedCumulativeGainAt(10)}"
-    )
-  }.foreach(println(_))
 
+  println("TESTING WITH TF WEIGHT")
+  val resTfIdf = evaluation(corpus, queryAndRelevanceSets, tfidf = false)
+  println("TESTING WITH TFIDF WEIGHT")
+  val resTf = evaluation(corpus, queryAndRelevanceSets, tfidf = true)
 }
