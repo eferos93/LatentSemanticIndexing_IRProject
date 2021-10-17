e30d1c9dd3584f23cf9ba43409e15f0816640d01
Eros Fabrici <eros.fabrici@gmail.com>
2021-10-17 17:15:08 +0200

    MovieSummariesTest

--- src/main/scala/MovieSummariesTest.scala
+++ src/main/scala/MovieSummariesTest.scala
@@ -2,6 +2,15 @@ package org.ir.project
 
 object MovieSummariesTest extends App {
   val corpus = readMovieCorpus()
-  val irSystem = IRSystem(corpus, 500, tfidf = true)
+  var irSystem = IRSystem(corpus, 100, tfidf = true)
+  irSystem.query("murder detective action")
+  irSystem.query("detective kill police")
+  irSystem.query("party student university school")
+  irSystem.query("ancient rome roman legions")
 
+  irSystem = IRSystem(corpus, 200, "index", tfidf = true)
+  irSystem.query("murder detective action")
+  irSystem.query("detective kill police")
+  irSystem.query("party student university school")
+  irSystem.query("ancient rome roman legions")
 }
