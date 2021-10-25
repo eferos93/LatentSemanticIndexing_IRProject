07398d72b7e9d5b6ae8995036873c1614f55099a
eferos93 <eros.fabrici@gmail.com>
2021-10-25 20:05:15 +0200

    refactor

--- src/main/scala/MovieSummaries400SingularValues.scala
+++ src/main/scala/MovieSummaries400SingularValues.scala
@@ -7,35 +7,31 @@ import org.apache.spark.sql.Dataset
 import java.nio.file.{Files, Paths}
 
 object MovieSummaries400SingularValues extends App {
-  val corpus: Dataset[Movie] = readMovieCorpus()
-  println("Spark Web UI:")
-  println(sparkContext.uiWebUrl)
-  val irSystem: IRSystem[Movie] =
+  def buildIrSystem(numberOfSingularValues: Int, corpus: Dataset[Movie]): IRSystem[Movie] = {
     if (Files.exists(Paths.get("index"))) {
       println("index found")
-
-      if (Files.exists(Paths.get("matrices"))) {
-        println("matrices found")
-        IRSystem(corpus, "index", "matrices")
-      } else {
-        IRSystem(corpus, 400, "index", tfidf = true)
-      }
+      IRSystem(corpus, numberOfSingularValues, "index", tfidf = true)
     } else {
       println("index not found")
-      IRSystem(corpus, 400, tfidf = true)
+      IRSystem(corpus, numberOfSingularValues, tfidf = true)
     }
-
-  if (Files.notExists(Paths.get("matrices"))) {
-    irSystem.saveIrSystem()
   }
 
-  println("Query: \"murder detective action\"")
-  irSystem.query("murder detective action")
-  println("\nQuery: \"detective kill police\"")
-  irSystem.query("detective kill police")
-  println("\nQuery: \"party student university school\"")
-  irSystem.query("party student university school")
-  println("\nQuery: \"ancient rome roman legions\"")
-  irSystem.query("ancient rome roman legions")
+  def queryIrSystem(irSystem: IRSystem[Movie], numberOfSingularValues: Int): Unit = {
+    println(s"QUERYING IR SYSTEM WITH $numberOfSingularValues SINGULAR VALUES")
+    println("Query: \"murder detective action\"")
+    irSystem.query("murder detective action")
+    println("\nQuery: \"detective kill police\"")
+    irSystem.query("detective kill police")
+    println("\nQuery: \"party student university school\"")
+    irSystem.query("party student university school")
+    println("\nQuery: \"ancient rome roman legions\"")
+    irSystem.query("ancient rome roman legions")
+  }
 
+  val corpus: Dataset[Movie] = readMovieCorpus()
+  println("Spark Web UI:")
+  sparkContext.uiWebUrl.foreach(println(_))
+  queryIrSystem(buildIrSystem(100, corpus), 100)
+  queryIrSystem(buildIrSystem(400, corpus), 400)
 }
